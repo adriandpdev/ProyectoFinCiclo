83f26b566ac19921eda48fce51179aa939caf0f6
adriandpdev <adri.2000@hotmail.com>
2021-03-09 21:17:10 +0100

    09/03 - 22h

--- app/src/main/java/com/example/proyectofinciclo/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/proyectofinciclo/ui/home/HomeFragment.java
@@ -9,21 +9,57 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.proyectofinciclo.R;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class HomeFragment extends Fragment {
 
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    TabItem tabtw,tabnews;
+    PagerController pagerAdapter;
     private HomeViewModel homeViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        tabLayout = v.findViewById(R.id.tabLayout);
+        viewPager = v.findViewById(R.id.viewPager);
+        tabtw = v.findViewById(R.id.tabtw);
+        tabnews = v.findViewById(R.id.tabnews);
+        pagerAdapter = new PagerController(getParentFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if(tab.getPosition()==0){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+                if(tab.getPosition()==1){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+            }
 
-        return root;
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        return v;
     }
 }
